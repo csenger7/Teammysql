134e36f1de5306d5f0347a3f00f57e33d8425658
csenger7 <lengyelcs@kkszki.hu>
2024-11-15 13:18:58 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team
+{
+    internal class Program
+    {
+        public static connect conn = new connect();
+        public static void GetAllData()
+        {
+            conn.Connection.Open();
+
+            string sql = "SELECT * FROM `player`";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                var player = new
+                {
+                    Id = dr.GetInt32(0),
+                    Name = dr.GetString(1),
+                    Height = dr.GetInt32(2),
+                    Weight = dr.GetInt32(3),
+                    CreatedTime = dr.GetDateTime(4),
+                };
+
+
+                
+
+                Console.WriteLine($"Játékos adatok: {player.Name}, {player.CreatedTime}");
+            }
+            dr.Close();
+            conn.Connection.Close();
+            
+            }
+            public static void Main(string[] args)
+        {
+            GetAllData();
+        }
+    }
+}
---
+++ connect.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team
+{
+    public class connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public connect() 
+        {
+            Host = "localhost";
+            Database = "Team";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+    
+    }
+
